d5edef011bda56391d1c98dadc5e7f41ad24f126
anupamn29 <anupamn29@gmail.com>
2026-06-25 00:42:59 +0530

    NeVA_Dropdown

--- Playwright_Project/tests/NeVa.locators.spec.ts
+++ Playwright_Project/tests/NeVa.locators.spec.ts
@@ -2,7 +2,8 @@ import {test,expect} from '@playwright/test';
 test("NeVa Title Check",async({page})=>{
 
     await page.goto("https://neva.gov.in/")
-    await expect (page).toHaveTitle("National e-Vidhan Application - Digital Legislators ")
+    await page.waitForLoadState('networkidle')
+    await expect(page).toHaveTitle(/National e-Vidhan Application - Digital Legislators/)
 
     const tablist=page.getByRole("tab",{name:"Council"})
     await expect(tablist).toBeVisible()
@@ -11,7 +12,7 @@ test("NeVa Title Check",async({page})=>{
     const tabCouncil=page.getByRole("link",{name:"Bihar"})
     await tabCouncil.click()
 
-    const welbla=page.getByText('Bihar Legislative Council ')
+    const welbla=page.getByText('Bihar Legislative Council')
     await expect(welbla).toBeVisible()
 
     const language=page.getByRole('link',{name:'English'})
@@ -30,4 +31,19 @@ test("NeVa Title Check",async({page})=>{
     await expect(assemblyOption).toBeVisible()
     await assemblyOption.click()
     await expect(asshouse).toHaveText('Bihar Legislative Council')
+
+
+    const BudgetSessionField = page.getByRole('combobox', { name: '212 Budget Session' });
+    await expect(BudgetSessionField).toBeVisible()
+    await BudgetSessionField.click()
+
+    const session = page.locator('li.select2-results__option')
+    await expect(session.first()).toBeVisible()
+    console.log(await session.nth(2).textContent())
+    if(await session.nth(2).textContent() === '210 Monsoon Session'){
+        await session.nth(2).click()
+    }
+    else{
+        console.error("Expected option not found")
+    }
 })
